0a098f93ba2c561ff46c8e81b58da467f02b88d5
Tamizharivu <tamizharivu.kumar@kovai.co>
2022-03-28 21:44:59 +0530

    Exception handling in SSC

--- Controllers/SkillSetController.cs
+++ Controllers/SkillSetController.cs
@@ -32,14 +32,22 @@ namespace Hire360WebAPI.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<SkillSet>> GetSkillSet(Guid id)
         {
-            var skillSet = await _context.SkillSets.FindAsync(id);
-
-            if (skillSet == null)
+            try
             {
-                return NotFound();
-            }
+                var skillSet = await _context.SkillSets.FindAsync(id);
 
-            return skillSet;
+                if (skillSet == null)
+                {
+                    return Ok(new { status = "Failed", data = skillSet, message = "No SkillSets Id found in the give Id" });
+                }
+
+                return skillSet;
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex);
+                return BadRequest(new { status = "failed", message = "Get SkillSets Failed" });
+            }
         }
 
         // PUT: api/SkillSet/5
@@ -47,30 +55,29 @@ namespace Hire360WebAPI.Controllers
         [HttpPut("{id}")]
         public async Task<IActionResult> PutSkillSet(Guid id, SkillSet skillSet)
         {
-            if (id != skillSet.SkillSetId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(skillSet).State = EntityState.Modified;
-
             try
             {
+                if (id != skillSet.SkillSetId)
+                {
+                    return Ok(new { status = "Failed", data = skillSet, message = "SkillSet Id not found" });
+                }
+
+                _context.Entry(skillSet).State = EntityState.Modified;
+
                 await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
             {
                 if (!SkillSetExists(id))
                 {
-                    return NotFound();
+                    return Ok(new { status = "Failed", data = skillSet, messsage = "SkillSet Id is already available" });
                 }
                 else
                 {
                     throw;
                 }
             }
-
-            return NoContent();
+            return Ok(new { status = "Failed", data = skillSet, messsage = "Failed to Update the SkillSet" });
         }
 
         // POST: api/SkillSet
@@ -78,26 +85,42 @@ namespace Hire360WebAPI.Controllers
         [HttpPost]
         public async Task<ActionResult<SkillSet>> PostSkillSet(SkillSet skillSet)
         {
-            _context.SkillSets.Add(skillSet);
-            await _context.SaveChangesAsync();
+            try
+            {
+                _context.SkillSets.Add(skillSet);
+                await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetSkillSet", new { id = skillSet.SkillSetId }, skillSet);
+                return CreatedAtAction("GetSkillSet", new { id = skillSet.SkillSetId }, Ok(new { data = skillSet }));
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex);
+                return BadRequest(new { status = "Failed", message = "Failed to create new SkillSet" });
+            }
         }
 
         // DELETE: api/SkillSet/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteSkillSet(Guid id)
         {
-            var skillSet = await _context.SkillSets.FindAsync(id);
-            if (skillSet == null)
+            try
             {
-                return NotFound();
+                var skillSet = await _context.SkillSets.FindAsync(id);
+                if (skillSet == null)
+                {
+                    return Ok(new { status = "Failed", data = skillSet, message = "SkillSets Id not found" });
+                }
+
+                _context.SkillSets.Remove(skillSet);
+                await _context.SaveChangesAsync();
+
+                return Ok(new { status = "Success", data = skillSet, messsage = "SkillSets Id has be deleted..." });
             }
-
-            _context.SkillSets.Remove(skillSet);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex);
+                return BadRequest(new { status = "Failed", message = "Faild to delete SkillSets" });
+            }
         }
 
         private bool SkillSetExists(Guid id)
